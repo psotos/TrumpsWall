2cfea7f58ecd82c3cb4daa8ca1efd03473843866
Peter Sotos <psotos@epicproportionstour.com>
2016-12-02 15:11:49 -0700

    Add files via upload

--- src/com/eptent/firewall/TrumpsWall.java
+++ src/com/eptent/firewall/TrumpsWall.java
@@ -6,15 +6,18 @@ package com.eptent.firewall;
 import org.apache.commons.cli.*;
 
 /**
- * @author psoto6
+ * Javadoc text.
  *
+ * @author Peter L Sotos
+ * This class bans domains for countries on a configurable "bad" list.
  */
 public class TrumpsWall {
+	final static Logger logger = LoggerFactory.getLogger(TrumpsWall.class);
 	private static Options options;
 	private static CommandLine cmd;
 
 	/**
-	 * @param args
+	 * @param args  the command line arguments, may be null
 	 */
 	public static void main(String[] args) {
 		setUpOptions();
@@ -23,11 +26,24 @@ public class TrumpsWall {
 
 	}
 
+/**
+* Javadoc text.
+*
+* This class sets up the Command Line argument options.
+*/
 	private static void setUpOptions() {
 		options = new Options();
 		options.addOption("v", false, "display current version");
 	}
 
+/**
+* Javadoc text.
+*
+* This class parses the command line according to the configuration created in
+* the setUpOptions() method.
+
+@param args the arguments parameter which was passed in from the command line, not null
+*/
 	private static void handleCommandLine(String[] args) {
 		CommandLineParser parser = new DefaultParser();
 		try {
