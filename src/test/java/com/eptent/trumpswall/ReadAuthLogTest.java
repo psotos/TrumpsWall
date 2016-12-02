2dddf37aa77e06372e7e337fbeea77514b51e822
Peter Sotos <psotos@epicproportionstour.com>
2016-12-02 15:10:58 -0700

    Add files via upload

---
+++ src/test/java/com/eptent/trumpswall/ReadAuthLogTest.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class ReadAuthLogTest {
+  
+}
